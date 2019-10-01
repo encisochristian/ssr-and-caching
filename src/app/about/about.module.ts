5fa99a944f004b17c5cc3036ab63eb8d48108374
encisochristian <encisochristianmalana@gmail.com>
2019-10-01 20:11:36 +0800

    first commit

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about.component';
+
+
+
+@NgModule({
+  declarations: [AboutComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: AboutComponent
+      }
+    ])
+  ],
+  exports: [AboutComponent]
+})
+export class AboutModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
